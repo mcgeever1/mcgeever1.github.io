852c5e87c346110e7da2e1950699536c799eadd9
Ethan McGeever <161373946+mcgeever1@users.noreply.github.com>
2025-12-14 00:34:56 -0500

    perma-dark!!!

--- assets/js/search-setup.js
+++ assets/js/search-setup.js
@@ -1,11 +1,7 @@
-let searchTheme = determineComputedTheme();
+// let searchTheme = determineComputedTheme();
 const ninjaKeys = document.querySelector("ninja-keys");
 
-if (searchTheme === "dark") {
-  ninjaKeys.classList.add("dark");
-} else {
-  ninjaKeys.classList.remove("dark");
-}
+ninjaKeys.classList.add("dark");
 
 const openSearchModal = () => {
   // collapse navbarNav if expanded on mobile
