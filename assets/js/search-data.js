51723efb509bdf8cb1e1d6310024f6eba6325389
mcgeever1 <161373946+mcgeever1@users.noreply.github.com>
2026-03-17 02:30:43 +0000

    Deploying to gh-pages from @ mcgeever1/mcgeever1.github.io@b3bc57bd416708023629e109a1323c35d280008c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,4 +14,9 @@ ninja.data = [{
           description: "Microloans and local intermediaries as an approach to constrained farm credit systems in Sub-Saharan Africa",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1-project";
+            },},{id: "projects-mtg-cascade",
+          title: 'MTG Cascade',
+          description: "A zero-dependency browser game that tests your knowledge of Magic the Gathering card popularity",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2-project";
             },},];
