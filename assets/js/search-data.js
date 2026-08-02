fe789206fc09ac67f4eb6eb98772370b7efc45e9
mcgeever1 <161373946+mcgeever1@users.noreply.github.com>
2026-08-02 21:26:19 +0000

    Deploying to gh-pages from @ mcgeever1/mcgeever1.github.io@9be0b687f9f08d07449f88e243c15fc10302028c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -19,4 +19,9 @@ ninja.data = [{
           description: "A zero-dependency browser game that tests your knowledge of Magic the Gathering card popularity",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2-project";
+            },},{id: "projects-simulating-bird-flock-patters-with-linear-algebra",
+          title: 'Simulating bird flock patters with linear algebra',
+          description: "Mapping Bird Murmations to xyz vectorspace as a &quot;non-math&quot; friendly approach to linear algebra.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3-project";
             },},];
