4883cfa592d46387c3cbc93d96b4b6a2dbb88c09
mcgeever1 <161373946+mcgeever1@users.noreply.github.com>
2026-08-04 03:53:04 +0000

    Deploying to gh-pages from @ mcgeever1/mcgeever1.github.io@8521357f5a9cd14e2bac61fb8b6213cf64cb303e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,9 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "projects-microloans-through-intermediaries",
-          title: 'Microloans through Intermediaries',
-          description: "Microloans and local intermediaries as an approach to constrained farm credit systems in Sub-Saharan Africa",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1-project";
-            },},{id: "projects-mtg-cascade",
+  },{id: "projects-mtg-cascade",
           title: 'MTG Cascade',
-          description: "A zero-dependency browser game that tests your knowledge of Magic the Gathering card popularity",
+          description: "A Magic the Gathering browser game that tests your knowledge of card popularity",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2-project";
             },},{id: "projects-simulating-bird-flock-patters-with-linear-algebra",
