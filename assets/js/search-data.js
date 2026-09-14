32da59625a8421ad9fdf8ec41b344ceb588100bf
mcgeever1 <161373946+mcgeever1@users.noreply.github.com>
2026-09-14 04:07:24 +0000

    Deploying to gh-pages from @ mcgeever1/mcgeever1.github.io@3bbfe1e9600599840a8cb920a505b7e4131f64d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "projects-simulating-bird-flock-patterns-with-linear-algebra",
           title: 'Simulating bird flock patterns with linear algebra',
-          description: "Mapping Bird Murmurations to xyz vectorspace as a &quot;non-math&quot; friendly approach to linear algebra",
+          description: "Mapping Bird Murmurations to XYZ vector space as a &quot;non-math&quot; friendly approach to linear algebra",
           section: "Projects",handler: () => {
               window.location.href = "/projects/birds";
             },},{id: "projects-cascade",
